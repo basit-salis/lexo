ad29b1917f039751169bfaae9c4ad75622532647
basit-salis <55243587+basit-salis@users.noreply.github.com>
2024-01-18 04:27:22 +0000

    Update test middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,14 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  let hasCookie = request.cookies.has("user");
+ /* let hasCookie = request.cookies.has("user");
   console.log('middleware',hasCookie);
   if (!hasCookie) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
   let response = NextResponse.next();
-  return response;
+  return response;*/
+  return true;
 }
 
 export const config = {
